a97c50ee23275aebab6ff1f65886bf26c1347184
Baptiste D <63651713+Bapt5@users.noreply.github.com>
2021-09-24 21:42:19 +0200

    ENTs

---
+++ ENTs.py
@@ -0,0 +1,16 @@
+from pronotepy.ent import *
+
+ents = {
+None: None,
+'': None,
+'atrium_sud': atrium_sud, # ENT Atrium Sud
+'ac_reims': ac_reims, # ENT AC Reims
+'occitanie_montpellier': occitanie_montpellier, # ENT Occitanie Montpellier
+'ac_reunion': ac_reunion, # ENT AC Reunion
+'ile_de_france': ile_de_france, # ENT Ile de France
+'paris_classe_numerique': paris_classe_numerique, # ENT PCN
+'ac_lyon': ac_lyon, # ENT Lyon
+'ac_orleans_tours': ac_orleans_tours, # ENT AC Orleans-Tours
+'monbureaunumerique': monbureaunumerique, # ENT MonBureauNumerique (Grand Est)
+'ent_essonne': ent_essonne
+}
